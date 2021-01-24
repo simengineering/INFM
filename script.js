10bcb316466d6c0a3f17a84bbe280738ea664f97
Matthew Hopper <76822419+simengineering@users.noreply.github.com>
2021-01-23 21:56:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ class MenuLinks extends React.Component {
         link: 'index2.html',
         icon: 'fa-pencil-square-o' },
       {
-        text: 'Github page',
+        text: 'Github',
         link: 'https://github.com/simengineering',
         icon: 'fa-github' },
       {
